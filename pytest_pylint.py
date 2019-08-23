c47687ccdf4005e0f9c7a8cc34b5e0d94957dd50
José L. Patiño <jose.lpa@gmail.com>
2019-08-23 16:29:46 +0100

    Fixed parameters differing in `PyLintItem.repr_failure` method.

--- pytest_pylint.py
+++ pytest_pylint.py
@@ -315,7 +315,7 @@ class PyLintItem(pytest.Item, pytest.File):
         # Update the cache if the item passed pylint.
         self.config.pylint.mtimes[self.nodeid] = self.__mtime
 
-    def repr_failure(self, excinfo):
+    def repr_failure(self, excinfo, style=None):
         """Handle any test failures by checkint that they were ours."""
         if excinfo.errisinstance(PyLintException):
             return excinfo.value.args[0]
