4d28b81951968a4c53adea5ac7f00d4d83595824
Carson Gee <x@carsongee.com>
2020-02-09 23:42:37 -0700

    Release 0.15.0 (#110)
    * Implemented option to output Pylint results to a reports file. (#100)
    
    * Implemented option to output Pylint results to a reports file.
    
    * Removed comment left in code.
    
    * Fixed string for Python 2.x support.
    
    * Fixed parameters differing in `PyLintItem.repr_failure` method.
    
    * Fixed file path in unit test.
    
    * Fixed unused argument `style` in `PylintItem.repr_failure`.
    
    * Fixed Python 2.7 Pylint error.
    
    * Improved `PyLintItem.runtest` function.
    
    * Disabling Pylint check due to Python 2 multiple inheritance differences.
    
    * Added support for newer docstring warning (#103)
    
    * Add support for Python 3.8 (#104)
    
    * Require Python 3.5+ (#106)
    
    This also bumps pytest's and pylint's minimal version to the first one
    with Python-3-only support.
    
    See also https://docs.pytest.org/en/latest/py27-py34-deprecation.html
    
    * Add ::PYLINT in pytest verbose mode (#101)
    
    * Dismiss a PytestUnknownMarkWarning
    
    * Add ::PYLINT in pytest verbose mode
    
    * Refactor and release notes (#109)
    
    * Removed passing pylint ignores (#111)
    
    Closes #105
    
    Co-authored-by: José L. Patiño <jose.lpa@gmail.com>
    Co-authored-by: Michael <michael-k@users.noreply.github.com>
    Co-authored-by: Yan QiDong <yanqd0@outlook.com>

---
+++ pytest_pylint/tests/test_util.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Unit testing module for pytest-pylint util.py module
+"""
+from pytest_pylint.util import get_rel_path, should_include_file
+
+
+def test_get_rel_path():
+    """
+    Verify our relative path function.
+    """
+    correct_rel_path = 'How/Are/You/blah.py'
+    path = '/Hi/How/Are/You/blah.py'
+    parent_path = '/Hi/'
+    assert get_rel_path(path, parent_path) == correct_rel_path
+
+    parent_path = '/Hi'
+    assert get_rel_path(path, parent_path) == correct_rel_path
+
+
+def test_should_include_path():
+    """
+    Files should only be included in the list if none of the directories on
+    it's path, of the filename, match an entry in the ignore list.
+    """
+    ignore_list = [
+        "first", "second", "third", "part", "base.py"
+    ]
+    # Default includes.
+    assert should_include_file("random", ignore_list) is True
+    assert should_include_file("random/filename", ignore_list) is True
+    assert should_include_file("random/other/filename", ignore_list) is True
+    # Basic ignore matches.
+    assert should_include_file("first/filename", ignore_list) is False
+    assert should_include_file("random/base.py", ignore_list) is False
+    # Part on paths.
+    assert should_include_file("part/second/filename.py", ignore_list) is False
+    assert should_include_file("random/part/filename.py", ignore_list) is False
+    assert should_include_file("random/second/part.py", ignore_list) is False
+    # Part as substring on paths.
+    assert should_include_file(
+        "part_it/other/filename.py", ignore_list
+    ) is True
+    assert should_include_file(
+        "random/part_it/filename.py", ignore_list
+    ) is True
+    assert should_include_file("random/other/part_it.py", ignore_list) is True
+
+
+def test_pylint_ignore_patterns():
+    """Test if the ignore-patterns is working"""
+    ignore_patterns = [
+        "first.*",
+        ".*second",
+        "^third.*fourth$",
+        "part",
+        "base.py"
+    ]
+
+    # Default includes
+    assert should_include_file("random", [], ignore_patterns) is True
+    assert should_include_file("random/filename", [], ignore_patterns) is True
+    assert should_include_file(
+        "random/other/filename", [], ignore_patterns
+    ) is True
+
+    # Pattern matches
+    assert should_include_file("first1", [], ignore_patterns) is False
+    assert should_include_file("first", [], ignore_patterns) is False
+    assert should_include_file("_second", [], ignore_patterns) is False
+    assert should_include_file("second_", [], ignore_patterns) is False
+    assert should_include_file("second_", [], ignore_patterns) is False
+    assert should_include_file("third fourth", [], ignore_patterns) is False
+    assert should_include_file("_third fourth_", [], ignore_patterns) is True
+    assert should_include_file("part", [], ignore_patterns) is False
+    assert should_include_file("1part2", [], ignore_patterns) is True
+    assert should_include_file("base.py", [], ignore_patterns) is False
---
+++ pytest_pylint/util.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Utility functions for gathering files, etc.
+"""
+import re
+from os import sep
+
+
+class PyLintException(Exception):
+    """Exception to raise if a file has a specified pylint error"""
+
+
+def get_rel_path(path, parent_path):
+    """
+    Give the path to object relative to ``parent_path``.
+    """
+    replaced_path = path.replace(parent_path, '', 1)
+    if replaced_path[0] == sep:
+        rel_path = replaced_path[1:]
+    else:
+        rel_path = replaced_path
+    return rel_path
+
+
+def should_include_file(path, ignore_list, ignore_patterns=None):
+    """Checks if a file should be included in the collection."""
+    if ignore_patterns:
+        for pattern in ignore_patterns:
+            if re.match(pattern, path):
+                return False
+    parts = path.split(sep)
+    return not set(parts) & set(ignore_list)
